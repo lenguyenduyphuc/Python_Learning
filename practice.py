00e67133c94df7fe9c75014ee7840e91e4202368
Phuc <duyphuclenguyen@gmail.com>
2025-04-09 23:20:49 -0400

    Updated

--- practice.py
+++ practice.py
@@ -1,34 +1,20 @@
-class Vehicle:
-    def __init__(self):
-        self.speed = 0
-
-    def set_speed(self, speed_to_set):
-        self.speed = speed_to_set
-
-    def print_speed(self):
-        print(self.speed)
-
-
-class Car(Vehicle):
-    def print_car_speed(self):
-        print('Speed: ', end = '')
-        self.print_speed()
-
-
-class ElectricCar(Car):
-    def __init__(self):
-        self.battery_level = 0
-
-    def set_battery_level(self, level_to_set):
-        self.battery_level = level_to_set
-
-    def print_battery_level(self):
-        print(f'Battery: {self.battery_level}')
-
-
-myCar = ElectricCar()
-myCar.set_speed(20)
-myCar.set_battery_level(80)
-
-myCar.print_car_speed()
-myCar.print_battery_level()	
+user_input = input()
+while user_input != 'end':
+    try:
+        # Possible ValueError
+        divisor = int(user_input)
+        if divisor < 0:
+            # Possible NameError because
+            # compute() is not defined
+            print(compute(divisor))
+        else:
+            # Possible ZeroDivisionError
+            print(20 // divisor)     # Truncates to an integer
+    except ValueError:
+        print('v')
+    except ZeroDivisionError:
+        print('z')
+    except:
+        print('x')
+    user_input = input()
+print('OK')
